655b093384fee2366af8c2c961a41f37b82f9c02
Leszek <ziege4@gmail.com>
2017-03-16 19:53:45 +0100

    notifications

--- WeatherApp/app/src/main/java/com/example/rent/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/rent/weatherapp/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.rent.weatherapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -47,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText editText;
 
     private Retrofit retrofit;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,12 +97,44 @@ public class MainActivity extends AppCompatActivity {
 
                         showImageBySkyText(weatherDetails.getSkyText());
 
+                        if (progressDialog != null) {
+                            progressDialog.hide();
+                        }
+
+                        showNotification(searchQuery);
                     }
                 });
     }
 
+    private void showNotification(String searchQuery) {
+
+        PendingIntent mainActivityPendingIntent = PendingIntent.getActivity
+                (this, 11, new Intent(this, MainActivity.class), PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_mood);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentText("Załadowano informacje pogodowe dla miasta " + searchQuery)
+                .setSmallIcon(R.drawable.ic_mood)
+                .setFullScreenIntent(mainActivityPendingIntent, false)
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+//                .setBigContentTitle("BIG CONTENT TITLE").setSummaryText("SUMMARY TEXT"))
+                .setContentIntent(mainActivityPendingIntent)
+                .addAction(R.drawable.ic_search, "Search", mainActivityPendingIntent)
+                .addAction(R.drawable.ic_search, "Second", mainActivityPendingIntent)
+                .addAction(R.drawable.ic_search, "Third", mainActivityPendingIntent)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(searchQuery.hashCode(), notification);
+
+
+    }
+
     @OnClick(R.id.search_button)
     void OnSearchButton() {
+        progressDialog = ProgressDialog.show(this, "Ladowanko", "Sie laduje", true);
         search(editText.getText().toString());
     }
 
